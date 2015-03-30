95e00bf48907299f9e3e534ccc870f6ea9e885fb
Ben Peggs <b.peggs@googlemail.com>
2015-03-30 13:48:02 +0100

    "test" on command line now runs tests.

--- Lark/Main.cpp
+++ Lark/Main.cpp
@@ -1,12 +1,16 @@
 
+#include <Rk/string_ref.hpp>
+
 #define CATCH_CONFIG_RUNNER
 #include <catch.hpp>
 
 namespace Lark {
-  extern "C" int main () {
-    Catch::Session session;
-    session.configData ().showSuccessfulTests = false;
-    return session.run ();
+  extern "C" int main (int arg_count, char** args) {
+    if (arg_count > 1 && Rk::cstring_ref (args [1]) == "test") {
+      Catch::Session session;
+      session.configData ().showSuccessfulTests = false;
+      return session.run ();
+    }
   }
 
 }
